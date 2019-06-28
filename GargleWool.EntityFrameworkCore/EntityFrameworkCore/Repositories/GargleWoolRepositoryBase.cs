40275d4c3a34b913a900962a02ee114b1335c1df
zhutoutou <30338453+zhutoutou@users.noreply.github.com>
2019-06-28 11:32:36 +0800

    Ioc设置

---
+++ GargleWool.EntityFrameworkCore/EntityFrameworkCore/Repositories/GargleWoolRepositoryBase.cs
@@ -0,0 +1,28 @@
+﻿using Abp.Domain.Entities;
+using Abp.EntityFrameworkCore;
+using Abp.EntityFrameworkCore.Repositories;
+
+namespace GargleWool.EntityFrameworkCore.EntityFrameworkCore.Repositories
+{
+    public abstract class GargleWoolRepositoryBase<TEntity, TPrimaryKey> : EfCoreRepositoryBase<GargleWoolDbContext, TEntity, TPrimaryKey>
+        where TEntity : class, IEntity<TPrimaryKey>
+    {
+        public GargleWoolRepositoryBase(IDbContextProvider<GargleWoolDbContext> dbContextProvider)
+            : base(dbContextProvider)
+        {
+        }
+
+
+    }
+
+    public abstract class GargleWoolRepositoryBase<TEntity> : EfCoreRepositoryBase<GargleWoolDbContext, TEntity, int>
+       where TEntity : class, IEntity<int>
+    {
+        public GargleWoolRepositoryBase(IDbContextProvider<GargleWoolDbContext> dbContextProvider)
+            : base(dbContextProvider)
+        {
+        }
+
+
+    }
+}
